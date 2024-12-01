9968ffe7b8c55f7b2d67b74d10e146a95518b3b5
Elijah Day <>
2024-12-01 03:50:12 -0600

    Reformat code with clang-format

--- tui/src/driver.h
+++ tui/src/driver.h
@@ -1,32 +1,28 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-typedef struct driver_t
-{
+typedef struct driver_t {
 	FILE *serial_dev;
-}
-driver_t;
-
-typedef enum
-{
-	CMD_BYTE_DISABLE		=	0x00,
-	CMD_BYTE_ENABLE			=	0x01,
-	CMD_BYTE_REPORT_PORT	=	0xD0,
-	CMD_BYTE_PORT0			=	0x00,
-	CMD_BYTE_PORT1			=	0x01,
-	CMD_BYTE_SET_MODE		=	0xF4,
-	CMD_BYTE_SET_STATE		=	0xF5,
-}
-cmd_bytes_t;
+} driver_t;
 
 typedef enum {
-	INPUT			=	0x00,
-	QUTPUT			=	0x01,
-	QUTPUT_PULLUP	=	0x0B,
+	CMD_BYTE_DISABLE = 0x00,
+	CMD_BYTE_ENABLE = 0x01,
+	CMD_BYTE_REPORT_PORT = 0xD0,
+	CMD_BYTE_PORT0 = 0x00,
+	CMD_BYTE_PORT1 = 0x01,
+	CMD_BYTE_SET_MODE = 0xF4,
+	CMD_BYTE_SET_STATE = 0xF5,
+} cmd_bytes_t;
+
+typedef enum {
+	INPUT = 0x00,
+	QUTPUT = 0x01,
+	QUTPUT_PULLUP = 0x0B,
 } PIN_MODE_t;
 
 // Initialize the device driver.  Returns true on init success and false on init
--- tui/src/main.c
+++ tui/src/main.c
@@ -23,49 +23,40 @@ static tui_label_t tui_labels[] = {
 };
 
 static tui_button_t tui_buttons[] = {
-	{2, 9, "[Pin 2]", false, &no_func, &nc_func}
-};
+	{2, 9, "[Pin 2]", false, &no_func, &nc_func}};
 
-static tui_lamp_t tui_lamps[] = {
-	{21, 9, "[Pin 4]", false}
-};
+static tui_lamp_t tui_lamps[] = {{21, 9, "[Pin 4]", false}};
 
-static void update_lamps(void)
-{
-	if(getInput(&driver, 0x04))
-	{
+static void update_lamps(void) {
+	if (getInput(&driver, 0x04)) {
 		tui_lamps[0].active = true;
-	}
-	else
-	{
+	} else {
 		tui_lamps[0].active = false;
 	}
 }
 
 int main(int argc, char *argv[]) {
-	
+
 	/* Check for necessary args. */
-	
-	if(argc < 2)
-	{
+
+	if (argc < 2) {
 		printf("Too few arguments... need to specify a serial device path\n");
 		printf("Use format: main <path>\n");
 	}
-	
+
 	/*
 	 * Try to open the serial device first.  If we can't do this, then there's
 	 * no reason to start the TUI...
 	 */
-	
-	if(init_driver(&driver, argv[1]))
-	{
+
+	if (init_driver(&driver, argv[1])) {
 		/* Set pin modes. */
-		
+
 		setPinMode(&driver, 0x02, QUTPUT); /* TODO: Arbitrary pins for test. */
 		setPinMode(&driver, 0x04, INPUT);
-	
+
 		/* Add TUI components. */
-	
+
 		add_tui_labels(&tui, tui_labels, sizeof(tui_labels));
 		add_tui_buttons(&tui, tui_buttons, sizeof(tui_buttons));
 		add_tui_lamps(&tui, tui_lamps, sizeof(tui_lamps));
@@ -75,7 +66,7 @@ int main(int argc, char *argv[]) {
 		init_tui(&tui);
 
 		while (tui.running) {
-			update_lamps();		/* TODO: Temporary function for testing. */
+			update_lamps(); /* TODO: Temporary function for testing. */
 			draw_tui(&tui);
 			handle_tui_controls(&tui);
 			execute_tui_outputs(&tui);
@@ -85,9 +76,9 @@ int main(int argc, char *argv[]) {
 
 		exit_tui();
 	}
-	
+
 	/* Exit driver and end program. */
-	
+
 	exit_driver(&driver);
 	return 0;
 }
