b9d89fc0eab131c0822c87473d20351a26e40079
Elijah Day <>
2024-12-01 03:37:00 -0600

    Add driver output functionality

---
+++ tui/src/driver.c
@@ -0,0 +1,151 @@
+#include "driver.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool init_driver(driver_t *driver, const char *dev_path)
+{
+	bool init_success = false;
+
+	driver->serial_dev = NULL;
+	driver->serial_dev = fopen(dev_path, "wb");
+	
+	if(driver->serial_dev == NULL)
+	{
+		printf("Unable to open device at: %s\n", dev_path);
+	}
+	else
+	{
+		init_success = true;
+	}
+	
+	return init_success;
+}
+
+void exit_driver(driver_t *driver)
+{
+	if(driver->serial_dev)
+	{
+		fclose(driver->serial_dev);
+	}
+	else
+	{
+		printf("No device to close...\n");
+	}
+}
+
+/* This function arranges bytes into the Firmata/MIDI sequence needed. */
+static uint32_t format_firmata_bytes(int n_ubytes, uint8_t ubytes[n_ubytes])
+{
+	uint32_t fbytes = 0;
+
+	for(uint8_t i = 0; i < n_ubytes - 1; i++)
+	{
+		fbytes += ubytes[i];
+		fbytes = fbytes << 8;
+	}
+	
+	fbytes += ubytes[n_ubytes - 1];
+	
+	return fbytes;
+}
+
+void setQutput(driver_t *driver, uint8_t pin, bool state)
+{
+	uint32_t firmata_bytes = 0;
+	uint8_t ubytes[] = {state, pin, CMD_BYTE_SET_STATE};
+	firmata_bytes = format_firmata_bytes(3, ubytes);
+	fwrite(&firmata_bytes, 3, 1, driver->serial_dev);
+
+	/* Old, hardcoded stuff is below... Leave it here just in case. */
+	
+	#if 0	
+	
+	/* Set first byte: pin state... then shift to next byte. */
+	firmata_bytes += state;
+	firmata_bytes = firmata_bytes << 8;
+	
+	/* Set second byte: pin number... then shift to next byte. */
+	firmata_bytes += pin;
+	firmata_bytes = firmata_bytes << 8;
+	
+	/* Set third byte: command to set state. */
+	firmata_bytes += DRIVER_BYTE_SET_STATE;
+	
+	/* Send bytes to the serial device. */
+	fwrite(&firmata_bytes, 3, 1, driver->serial_dev);
+	
+	#endif
+}
+
+bool getInput(driver_t *driver, uint8_t pin)
+{
+	uint32_t firmata_bytes = 0;
+	uint32_t pin_bit;
+	
+	/*
+	 * SEND OVER CMD TO ENABLE REPORTING DIGITAL PINS...
+	 *
+	 * Stick to just port 0 right now for testing purposes...
+	 *
+	 * I think this is how reading pins works?  It doesn't look like there's a
+	 * way to read pins individually... TODO TODO TODO
+	 */
+	uint8_t ubytes[] = {CMD_BYTE_ENABLE, CMD_BYTE_REPORT_PORT | CMD_BYTE_PORT0};
+	firmata_bytes = format_firmata_bytes(2, ubytes);
+	fwrite(&firmata_bytes, 2, 1, driver->serial_dev);
+	
+	/*
+	 * Then I *think* this is where the digital pin values get read...
+	 *
+	 * It says that a port covers 8 bits, so assume we only receive one byte?
+	 * TODO
+	 */
+	firmata_bytes = 0;
+	fread(&firmata_bytes, 1, 1, driver->serial_dev);
+	
+	/*
+	 * After reading, we can AND the values read from the port with a "1" at the
+	 * position of the pin that we want...
+	 *
+	 * (I'm assuming the pins start at pin 0 from "port 0...")
+	 */
+	 pin_bit = 0x01 << pin;
+	 
+	 if((firmata_bytes & pin_bit) > 0)
+	 {
+	 	return true;
+	 }
+	 else
+	 {
+	 	return false;
+	 }
+}
+
+void setPinMode(driver_t *driver, uint8_t pin, PIN_MODE_t pinMode)
+{
+	uint32_t firmata_bytes = 0;
+	uint8_t ubytes[] = {pinMode, pin, CMD_BYTE_SET_MODE};
+	firmata_bytes = format_firmata_bytes(3, ubytes);
+	fwrite(&firmata_bytes, 3, 1, driver->serial_dev);
+	
+	/* Old, hardcoded stuff is below... Leave it here just in case. */
+	
+	#if 0
+	
+	/* Set first byte: pin mode... then shift to next byte. */
+	firmata_bytes += pinMode;
+	firmata_bytes = firmata_bytes << 8;
+
+	/* Set second byte: pin number... then shift to next byte. */
+	firmata_bytes += pin;
+	firmata_bytes = firmata_bytes << 8;
+	
+	/* Set third byte: command to set mode. */
+	firmata_bytes += DRIVER_BYTE_SET_MODE;
+	
+	/* Send bytes to the serial device. */
+	fwrite(&firmata_bytes, 3, 1, driver->serial_dev);
+	
+	#endif
+}
--- tui/src/main.c
+++ tui/src/main.c
@@ -1,53 +1,93 @@
+#include "driver.h"
 #include "tui.h"
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-/*
- * TODO: This is just a template for a possible TUI setup for the demo.  It'll
- * probably need to change between now and when we implement Firmata...
- */
-
+static driver_t driver;
 static tui_t tui;
 
-static void no_func(void) { tui.lamps[0].active = true; }
+static void no_func(void) { setQutput(&driver, 0x02, 0x00); }
 
-static void nc_func(void) { tui.lamps[0].active = false; }
+static void nc_func(void) { setQutput(&driver, 0x02, 0x01); }
 
 static tui_label_t tui_labels[] = {
-	{1, 1, "Physical Qutputs"},
-	{1, 2, "(Write Only)"},
-	{20, 1, "Physical Inputs"},
-	{20, 2, "(Read Only)"},
+	{2, 1, "Team Redstoners"},
+	{2, 2, "Wirecraft - Firmata Demo"},
+	{1, 4, "===================================="},
+	{2, 6, "Physical Qutputs"},
+	{2, 7, "(Write Only)"},
+	{21, 6, "Physical Inputs"},
+	{21, 7, "(Read Only)"},
 };
 
 static tui_button_t tui_buttons[] = {
-	{1, 4, "[GPIO1 ]", false, &no_func, &nc_func},
-	{1, 5, "[GPIO2 ]", false, NULL, NULL},
-	{1, 6, "[GPIO3 ]", false, NULL, NULL},
-	{1, 7, "[GPIO4 ]", false, NULL, NULL},
-	{1, 8, "[GPIO5 ]", false, NULL, NULL},
+	{2, 9, "[Pin 2]", false, &no_func, &nc_func}
 };
 
 static tui_lamp_t tui_lamps[] = {
-	{20, 4, "[GPIO6 ]", false}, {20, 5, "[GPIO7 ]", false},
-	{20, 6, "[GPIO8 ]", false}, {20, 7, "[GPIO9 ]", false},
-	{20, 8, "[GPIO10]", false},
+	{21, 9, "[Pin 4]", false}
 };
 
-int main(void) {
-	add_tui_labels(&tui, tui_labels, sizeof(tui_labels));
-	add_tui_buttons(&tui, tui_buttons, sizeof(tui_buttons));
-	add_tui_lamps(&tui, tui_lamps, sizeof(tui_lamps));
-
-	init_tui(&tui);
-
-	while (tui.running) {
-		draw_tui(&tui);
-		handle_tui_controls(&tui);
-		execute_tui_outputs(&tui);
+static void update_lamps(void)
+{
+	if(getInput(&driver, 0x04))
+	{
+		tui_lamps[0].active = true;
 	}
+	else
+	{
+		tui_lamps[0].active = false;
+	}
+}
 
-	exit_tui();
+int main(int argc, char *argv[]) {
+	
+	/* Check for necessary args. */
+	
+	if(argc < 2)
+	{
+		printf("Too few arguments... need to specify a serial device path\n");
+		printf("Use format: main <path>\n");
+	}
+	
+	/*
+	 * Try to open the serial device first.  If we can't do this, then there's
+	 * no reason to start the TUI...
+	 */
+	
+	if(init_driver(&driver, argv[1]))
+	{
+		/* Set pin modes. */
+		
+		setPinMode(&driver, 0x02, QUTPUT); /* TODO: Arbitrary pins for test. */
+		setPinMode(&driver, 0x04, INPUT);
+	
+		/* Add TUI components. */
+	
+		add_tui_labels(&tui, tui_labels, sizeof(tui_labels));
+		add_tui_buttons(&tui, tui_buttons, sizeof(tui_buttons));
+		add_tui_lamps(&tui, tui_lamps, sizeof(tui_lamps));
+
+		/* Init TUI and begin main loop. */
+
+		init_tui(&tui);
+
+		while (tui.running) {
+			update_lamps();		/* TODO: Temporary function for testing. */
+			draw_tui(&tui);
+			handle_tui_controls(&tui);
+			execute_tui_outputs(&tui);
+		}
+
+		/* Exit TUI. */
+
+		exit_tui();
+	}
+	
+	/* Exit driver and end program. */
+	
+	exit_driver(&driver);
+	return 0;
 }
