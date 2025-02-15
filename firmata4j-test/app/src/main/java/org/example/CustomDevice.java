49ae29feedde3ec76875ea9aa2fd7477bb650202
Noorquacker <noorquacker@nqind.com>
2025-02-15 09:12:00 -0600

    Add firmata4j test app

---
+++ firmata4j-test/app/src/main/java/org/example/CustomDevice.java
@@ -0,0 +1,505 @@
+package org.example;
+
+
+import org.firmata4j.firmata.FirmataDevice;
+import org.firmata4j.Consumer;
+import org.firmata4j.I2CDevice;
+import org.firmata4j.IODeviceEventListener;
+import org.firmata4j.IOEvent;
+import org.firmata4j.Parser;
+import org.firmata4j.Pin;
+import org.firmata4j.firmata.DaemonThreadFactory;
+import org.firmata4j.firmata.FirmataI2CDevice;
+import org.firmata4j.firmata.FirmataMessageFactory;
+import org.firmata4j.firmata.FirmataPin;
+import org.firmata4j.firmata.parser.FirmataParser;
+import org.firmata4j.firmata.parser.WaitingForMessageState;
+import org.firmata4j.fsm.Event;
+import org.firmata4j.fsm.FiniteStateMachine;
+import org.firmata4j.transport.SerialTransport;
+import org.firmata4j.transport.TransportInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import static org.firmata4j.firmata.parser.FirmataEventType.*;
+import static org.firmata4j.firmata.parser.FirmataToken.*;
+
+/**
+ * Implements {@link IODevice} that is using Firmata protocol.
+ *
+ * @author Oleg Kurbatov &lt;o.v.kurbatov@gmail.com&gt;
+ */
+public class CustomDevice implements org.firmata4j.IODevice {
+
+    private Parser parser;
+    private TransportInterface transport;
+    private FiniteStateMachine protocol;
+    private final Set<IODeviceEventListener> listeners = Collections.synchronizedSet(new LinkedHashSet<IODeviceEventListener>());
+    private final List<FirmataPin> pins = Collections.synchronizedList(new ArrayList<FirmataPin>());
+    private final Queue<Byte> pinStateRequestQueue = new ArrayDeque<>();
+    private final AtomicBoolean started = new AtomicBoolean(false);
+    private final AtomicBoolean ready = new AtomicBoolean(false);
+    private final AtomicInteger initializedPins = new AtomicInteger(0);
+    private final AtomicInteger longestI2CDelay = new AtomicInteger(0);
+    private final Map<Byte, FirmataI2CDevice> i2cDevices = new HashMap<>();
+    private volatile Map<String, Object> firmwareInfo;
+    private volatile Map<Integer, Integer> analogMapping;
+    
+    private static final long TIMEOUT = 15000L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomDevice.class);
+
+    /**
+     * Constructs FirmataDevice instance on specified serial port.
+     *
+     * @param portName the serial port name the device is connected to
+     */
+    public CustomDevice(String portName) {
+        this(new SerialTransport(portName));
+    }
+
+    /**
+     * Constructs FirmataDevice instance that operates on default protocol
+     * using specified transport.
+     *
+     * @param transport the communication channel
+     */
+    public CustomDevice(TransportInterface transport) {
+        protocol = new FiniteStateMachine(WaitingForMessageState.class);
+        protocol.setEventHandlingExecutor(Executors.newSingleThreadExecutor(new DaemonThreadFactory("firmata-event-handler")));
+        protocol.addHandler(PROTOCOL_MESSAGE, onProtocolReceive);
+        protocol.addHandler(FIRMWARE_MESSAGE, onFirmwareReceive);
+        protocol.addHandler(PIN_CAPABILITIES_MESSAGE, onCapabilitiesReceive);
+        protocol.addHandler(PIN_CAPABILITIES_FINISHED, onCapabilitiesFinished);
+        protocol.addHandler(PIN_STATE, onPinStateReceive);
+        protocol.addHandler(ANALOG_MAPPING_MESSAGE, onAnalogMappingReceive);
+        protocol.addHandler(ANALOG_MESSAGE_RESPONSE, onAnalogMessageReceive);
+        protocol.addHandler(DIGITAL_MESSAGE_RESPONSE, onDigitalMessageReceive);
+        protocol.addHandler(I2C_MESSAGE, onI2cMessageReceive);
+        protocol.addHandler(STRING_MESSAGE, onStringMessageReceive);
+        protocol.addHandler(FiniteStateMachine.FSM_IS_IN_TERMINAL_STATE, new Consumer<Event>() {
+            @Override
+            public void accept(Event t) {
+                LOGGER.error("Parser has reached the terminal state. It may be due receiving of unsupported command.");
+            }
+        });
+        parser = new FirmataParser(protocol);
+        transport.setParser(parser);
+        this.transport = transport;
+    }
+
+    /**
+     * Constructs FirmataDevice instance using the specified transport and
+     * protocol.
+     *
+     * @param transport the communication channel
+     * @param protocol finite state machine that can understand the byte stream
+     * from the transport
+     */
+    public CustomDevice(TransportInterface transport, FiniteStateMachine protocol) {
+        parser = new FirmataParser(protocol);
+        transport.setParser(parser);
+        this.protocol = protocol;
+        this.transport = transport;
+    }
+
+    @Override
+    public void start() throws IOException {
+        if (!started.getAndSet(true)) {
+            /*
+             The startup strategy is to start device and immediately
+             send the REPORT_FIRMWARE message.  When we receive the
+             firmware name reply, then we know the board is ready to
+             communicate.
+
+             For boards like Arduino which use DTR to reset, they may
+             reboot the moment the port opens.  They will not hear this
+             REPORT_FIRMWARE message, but when they finish booting up
+             they will send the firmware message.
+
+             For boards that do not reboot when the port opens, they
+             will hear this REPORT_FIRMWARE request and send the
+             response.  If this REPORT_FIRMWARE request isn't sent,
+             these boards will not automatically send this info.
+
+             Either way, when we hear the REPORT_FIRMWARE reply, we
+             know the board is alive and ready to communicate.
+             */
+            try {
+                parser.start();
+                transport.start();
+                sendMessage(FirmataMessageFactory.REQUEST_FIRMWARE);
+            } catch (IOException ex) {
+                transport.stop();
+                parser.stop();
+                throw ex;
+            }
+        }
+    }
+
+    @Override
+    public void stop() throws IOException {
+        shutdown();
+        IOEvent event = new IOEvent(this);
+        for (IODeviceEventListener l : listeners) {
+            l.onStop(event);
+        }
+
+    }
+
+    @Override
+    public void ensureInitializationIsDone() throws InterruptedException {
+        if (!started.get()) {
+            try {
+                start();
+            } catch (IOException ex) {
+                throw new InterruptedException(ex.getMessage());
+            }
+        }
+        long timePassed = 0L;
+        long timeout = 100;
+        while (!isReady()) {
+            if (timePassed >= TIMEOUT) {
+                throw new InterruptedException("Connection timeout");
+            }
+            timePassed += timeout;
+            Thread.sleep(timeout);
+        }
+    }
+
+    @Override
+    public boolean isReady() {
+        return ready.get();
+    }
+
+    @Override
+    public void addEventListener(IODeviceEventListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void removeEventListener(IODeviceEventListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public Set<Pin> getPins() {
+        return new HashSet<Pin>(pins);
+    }
+
+    @Override
+    public int getPinsCount() {
+        return pins.size();
+    }
+
+    @Override
+    public Pin getPin(int index) {
+        return pins.get(index);
+    }
+
+    @Override
+    public synchronized I2CDevice getI2CDevice(byte address) throws IOException {
+        if (!i2cDevices.containsKey(address)) {
+            // COMMENT OUT
+            // i2cDevices.put(address, new FirmataI2CDevice(this, address));
+        }
+        sendMessage(FirmataMessageFactory.i2cConfigRequest(longestI2CDelay.get()));
+        return i2cDevices.get(address);
+    }
+
+    @Override
+    public String getProtocol() {
+        return MessageFormat.format(
+                "{0} - {1}.{2}",
+                firmwareInfo.get(FIRMWARE_NAME),
+                firmwareInfo.get(FIRMWARE_MAJOR),
+                firmwareInfo.get(FIRMWARE_MINOR));
+    }
+
+    @Override
+    public void addProtocolMessageHandler(String messageType, Consumer<Event> handler) {
+        protocol.addHandler(messageType, handler);
+    }
+    
+    //TODO add get firmware method
+
+    @Override
+    public void sendMessage(String message) throws IOException {
+        if (message.length() > 15) {
+            LOGGER.warn("Firmata 2.3.6 implementation has input buffer only 32 bytes so you can safely send only 15 characters log messages");
+        }
+        sendMessage(FirmataMessageFactory.stringMessage(message));
+    }
+
+    /**
+     * Sends raw message to connected Firmata device using open port.<br/>
+     * Consider using {@link FirmataMessageFactory} methods to prepare binary
+     * messages before sending.
+     *
+     * @param msg the Firmata message
+     * @throws IOException when writing fails
+     * @see FirmataMessageFactory
+     */
+    @Override
+    public void sendMessage(byte... msg) throws IOException {
+        transport.write(msg);
+    }
+
+    /**
+     * Notifies the device listeners that a pin has changed.<br/>
+     * This method is package-wide accessible to be used by {@link FirmataPin}.
+     *
+     * @param event the event to be send to the listeners
+     */
+    void pinChanged(IOEvent event) {
+        for (IODeviceEventListener listener : listeners) {
+            listener.onPinChange(event);
+        }
+    }
+
+    /**
+     * Sets delay between the moment an I2C device's register is written to and
+     * the moment when the data can be read from that register. The delay is set
+     * per firmata-device (not per I2C device). So firmata-device uses the
+     * longest delay.
+     *
+     * @param delay longest delay between writing to I2C and reading from it
+     * @throws IOException when sending of configuration to firmata-device
+     *                     failed
+     */
+    void setI2CDelay(int delay) throws IOException {
+        byte[] message = FirmataMessageFactory.i2cConfigRequest(delay);
+        int longestDelaySoFar = longestI2CDelay.get();
+        while (longestDelaySoFar < delay) {
+            if (longestI2CDelay.compareAndSet(longestDelaySoFar, delay)) {
+                sendMessage(message);
+            }
+            longestDelaySoFar = longestI2CDelay.get();
+        }
+    }
+
+    /**
+     * Tries to release all resources and properly terminate the connection to
+     * the hardware.
+     *
+     * @throws IOException when communication could not be stopped properly
+     */
+    private void shutdown() throws IOException {
+        ready.set(false);
+        sendMessage(FirmataMessageFactory.analogReport(false));
+        sendMessage(FirmataMessageFactory.digitalReport(false));
+        parser.stop();
+        transport.stop();
+    }
+
+    /**
+     * Describes reaction to protocol receiving.
+     */
+    private final Consumer<Event> onProtocolReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            if (!event.getBodyItem(PROTOCOL_MAJOR).equals((int) FIRMATA_MAJOR_VERSION)) {
+                LOGGER.error("Current version of firmata protocol on device ({}.{}) is not compatible with version of firmata4j ({}.{}).",
+                        event.getBodyItem(PROTOCOL_MAJOR),
+                        event.getBodyItem(PROTOCOL_MINOR),
+                        FIRMATA_MAJOR_VERSION,
+                        FIRMATA_MINOR_VERSION);
+            } else if (!event.getBodyItem(PROTOCOL_MINOR).equals((int) FIRMATA_MINOR_VERSION)) {
+                LOGGER.warn("Current version of firmata protocol on device ({}.{}) differs from version supported by firmata4j ({}.{})."
+                                + " Though these are compatible you may experience some issues.",
+                        event.getBodyItem(PROTOCOL_MAJOR),
+                        event.getBodyItem(PROTOCOL_MINOR),
+                        FIRMATA_MAJOR_VERSION,
+                        FIRMATA_MINOR_VERSION);
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to firmware data receiving.
+     */
+    private final Consumer<Event> onFirmwareReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            firmwareInfo = event.getBody();
+            try {
+                sendMessage(FirmataMessageFactory.REQUEST_CAPABILITY);
+            } catch (IOException ex) {
+                LOGGER.error("Error requesting device capabilities.", ex);
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to capabilities data receiving.
+     */
+    private final Consumer<Event> onCapabilitiesReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            byte pinId = (Byte) event.getBodyItem(PIN_ID);
+            FirmataPin pin = new FirmataPin(CustomDevice.super.this, pinId);
+            for (byte i : (byte[]) event.getBodyItem(PIN_SUPPORTED_MODES)) {
+                pin.addSupportedMode(Pin.Mode.resolve(i));
+            }
+            pins.add(pin.getIndex(), pin);
+            if (pin.getSupportedModes().isEmpty()) {
+                // if the pin has no supported modes, its initialization is already done
+                initializedPins.incrementAndGet();
+            } else {
+                // if the pin supports some modes, we ask for its current mode and value
+                pinStateRequestQueue.add(pinId);
+            }
+        }
+    };
+    
+    /**
+     * Called when capabilities for the all the pins are received.
+     */
+    private final Consumer<Event> onCapabilitiesFinished = new Consumer<Event>() {
+        @Override
+        public void accept(Event t) {
+            if (initializedPins.get() == pins.size()) {
+                try {
+                    sendMessage(FirmataMessageFactory.ANALOG_MAPPING_REQUEST);
+                } catch (IOException e) {
+                    LOGGER.error("Error requesting of the analog mapping", e);
+                }
+            } else {
+                byte pinId = pinStateRequestQueue.poll();
+                try {
+                    sendMessage(FirmataMessageFactory.pinStateRequest(pinId));
+                } catch (IOException ex) {
+                    LOGGER.error(String.format("Error requesting state of pin %d", pinId), ex);
+                }
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to the pin state data receiving.
+     */
+    private final Consumer<Event> onPinStateReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            byte pinId = (Byte) event.getBodyItem(PIN_ID);
+            FirmataPin pin = pins.get(pinId);
+            if (pin.getMode() == null) {
+                pin.initMode(Pin.Mode.resolve((Byte) event.getBodyItem(PIN_MODE)));
+                pin.initValue((Long) event.getBodyItem(PIN_VALUE));
+            } else {
+                pin.updateValue((Long) event.getBodyItem(PIN_VALUE));
+            }
+            if (!pinStateRequestQueue.isEmpty()) {
+                byte pid = pinStateRequestQueue.poll();
+                try {
+                    sendMessage(FirmataMessageFactory.pinStateRequest(pid)); // request the following pin state
+                } catch (IOException ex) {
+                    LOGGER.error(String.format("Error requesting state of pin %d", pid), ex);
+                }
+            }
+            if (initializedPins.incrementAndGet() == pins.size()) {
+                try {
+                    sendMessage(FirmataMessageFactory.ANALOG_MAPPING_REQUEST);
+                } catch (IOException e) {
+                    LOGGER.error("Error requesting of the analog mapping", e);
+                }
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to the analog mapping data receiving.
+     */
+    private final Consumer<Event> onAnalogMappingReceive = new Consumer<Event>() {
+        @Override
+        @SuppressWarnings("unchecked")
+        public void accept(Event event) {
+            analogMapping = (Map<Integer, Integer>) event.getBodyItem(ANALOG_MAPPING);
+            try {
+                sendMessage(FirmataMessageFactory.analogReport(true));
+                sendMessage(FirmataMessageFactory.digitalReport(true));
+            } catch (IOException ex) {
+                LOGGER.error("Cannot enable reporting from device", ex);
+            }
+            ready.set(true);
+            // all the pins are initialized so notification is sent to listeners
+            IOEvent initIsDone = new IOEvent(FirmataDevice.this);
+            for (IODeviceEventListener l : listeners) {
+                l.onStart(initIsDone);
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to the analog message data receiving.
+     */
+    private final Consumer<Event> onAnalogMessageReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            int analogId = (Integer) event.getBodyItem(PIN_ID);
+            if (analogMapping != null && analogMapping.get(analogId) != null) {
+                int pinId = analogMapping.get(analogId);
+                if (pinId < pins.size()) {
+                    FirmataPin pin = pins.get(pinId);
+                    if (Pin.Mode.ANALOG.equals(pin.getMode())) {
+                        pin.updateValue((Integer) event.getBodyItem(PIN_VALUE));
+                    }
+                }
+            }
+        }
+    };
+
+    /**
+     * Describes reaction to the digital message data receiving.
+     */
+    private final Consumer<Event> onDigitalMessageReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            int pinId = (Integer) event.getBodyItem(PIN_ID);
+            if (pinId < pins.size()) {
+                FirmataPin pin = pins.get(pinId);
+                if (Pin.Mode.INPUT.equals(pin.getMode()) ||
+                        Pin.Mode.PULLUP.equals(pin.getMode())) {
+                    pin.updateValue((Integer) event.getBodyItem(PIN_VALUE));
+                }
+            }
+        }
+    };
+
+    private final Consumer<Event> onI2cMessageReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            byte address = (Byte) event.getBodyItem(I2C_ADDRESS);
+            int register = (Integer) event.getBodyItem(I2C_REGISTER);
+            byte[] message = (byte[]) event.getBodyItem(I2C_MESSAGE);
+            FirmataI2CDevice device = i2cDevices.get(address);
+            if (device != null) {
+                device.onReceive(register, message);
+            }
+        }
+    };
+
+    private final Consumer<Event> onStringMessageReceive = new Consumer<Event>() {
+        @Override
+        public void accept(Event event) {
+            String message = (String) event.getBodyItem(STRING_MESSAGE);
+            IOEvent evt = new IOEvent(FirmataDevice.this);
+            for (IODeviceEventListener listener : listeners) {
+                listener.onMessageReceive(evt, message);
+            }
+        }
+    };
+}
---
+++ firmata4j-test/app/src/main/java/org/example/CustomTransport.java
@@ -0,0 +1,148 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
+
+import org.firmata4j.Parser;
+
+/**
+ * Allows connections over the serial interface.
+ *
+ * @author Ali Kia
+ */
+public class CustomTransport implements org.firmata4j.transport.TransportInterface {
+    private Parser parser;
+    private Thread readerThread;
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private final InetAddress ip;
+    private final int port;
+
+    private Logger log;
+
+    /**
+     * Creates network transport using a sting as address.
+     *
+     * Address should specify host and port. Examples:
+     * <ul>
+     *   <li>"192.168.1.10:36363"</li>
+     *   <li>"explorer-bot.local:72727"</li>
+     * </ul>
+     * @param address host and port
+     */
+    public CustomTransport(String address) {
+        String[] parts = address.split(":");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Address must specify host and port but received " + address);
+        }
+        try {
+            this.ip = InetAddress.getByName(parts[0]);
+        } catch (UnknownHostException e) {
+            throw new IllegalArgumentException(e);
+        }
+        this.port = Integer.valueOf(parts[1]);
+        log = Logger.getLogger("FirmataTest");
+        log.info("HELLO");
+    }
+    
+    public CustomTransport(InetAddress ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        log = Logger.getLogger("FirmataTest");
+        log.info("HELLO");
+    }
+
+    @Override
+    public void start() throws IOException {
+        socket = new Socket(ip, port);
+        log.info("socket made");
+        socket.setReuseAddress(true);
+        log.info("setReuseAddress made");
+        socket.setSoTimeout(1500);
+        log.info("setSoTimeout made");
+        socket.setSoLinger(true, 1500);
+        log.info("setSoLinger made");
+        socket.setSoTimeout(1500);
+        log.info("setSoTimeout made");
+        out = new DataOutputStream(socket.getOutputStream());
+        log.info("DataOutputStream made");
+        in = new DataInputStream(socket.getInputStream());
+        log.info("DataInputStream made");
+        readerThread = new Thread(new Reader(), "firmata-network-transport");
+        log.info("readerThread made");
+        readerThread.start();
+        log.info("readerThread.start made");
+    }
+
+    @Override
+    public void stop() throws IOException {
+        try {
+            readerThread.interrupt();
+            readerThread.join();
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+        }
+        try {
+            if (out != null) {
+                out.close();
+            }
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } finally {
+                if (socket != null) {
+                    socket.close();
+                }
+            }
+        }
+        out = null;
+        in = null;
+        socket = null;
+
+    }
+
+    @Override
+    public void write(byte[] bytes) throws IOException {
+        out.write(bytes);
+    }
+
+    @Override
+    public void setParser(Parser parser) {
+        this.parser = parser;
+    }
+
+    private class Reader implements Runnable {
+
+        @Override
+        public void run() {
+            byte[] buf = new byte[100];
+            int readIn;
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    readIn = in.read(buf);
+                } catch (SocketTimeoutException e) {
+                    break;  // We try to reconnect, hearthbeats (1*second) missing
+                } catch (IOException e) {
+                    break;
+                }
+                if (readIn == -1) {
+                    break;  // Connection closed
+                }
+                byte[] data = new byte[readIn];
+                System.arraycopy(buf, 0, data, 0, readIn);
+                parser.parse(data);
+            }
+        }
+
+    }
+
+}
